d811e4cb68be79b006d051cf9a36fac1aefee875
rachit-developer24 <srachit841@gmail.com>
2025-07-19 23:25:33 +0100

    Initial Commit

---
+++ CrptoProjectApi/CrptoProjectApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrptoProjectApiApp.swift
+//  CrptoProjectApi
+//
+//  Created by Rachit Sharma on 19/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct CrptoProjectApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CrptoProjectApiTests/CrptoProjectApiTests.swift
@@ -0,0 +1,17 @@
+//
+//  CrptoProjectApiTests.swift
+//  CrptoProjectApiTests
+//
+//  Created by Rachit Sharma on 19/07/2025.
+//
+
+import Testing
+@testable import CrptoProjectApi
+
+struct CrptoProjectApiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
